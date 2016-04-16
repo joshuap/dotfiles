acf6d2365ce2953b4ddffa05139d17a5656f396c
Joshua Wood <josh@hintmedia.com>
2016-04-16 11:45:52 -0700

    Add gs='git status' alias.

--- oh-my-zsh/custom/plugins/joshuap/joshuap.plugin.zsh
+++ oh-my-zsh/custom/plugins/joshuap/joshuap.plugin.zsh
@@ -1,14 +1,18 @@
-# Bundler aliases
-alias be="bundle exec"
-alias bi="bundle install"
-alias bl="bundle list"
-alias bp="bundle package"
-alias bu="bundle update"
+alias be='bundle exec'
+alias bi='bundle install'
+alias bl='bundle list'
+alias bp='bundle package'
+alias bu='bundle update'
 alias ber='bundle exec rake'
-alias gap='git add -p'
-alias got="go test"
 
-alias cl="clear"
+alias cl='clear'
+
+# This replaces the ghostscript `gs` command on some systems, which is still
+# available via `ghostscript`.
+alias gs='git status'
+
+alias gap='git add -p'
+alias got='go test'
 
 bvim() { vim `bundle show $1`; }
 
