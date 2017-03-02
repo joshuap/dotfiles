b371e10f18fd57065cb071ac9415268028aa05a6
Joshua Wood <josh@hintmedia.com>
2017-03-02 05:24:35 -0800

    Layout tweaks and adding additional keys.

--- tag-arch/xmonad/xmonad.hs
+++ tag-arch/xmonad/xmonad.hs
@@ -1,15 +1,27 @@
+{-# LANGUAGE AllowAmbiguousTypes, DeriveDataTypeable, TypeSynonymInstances, MultiParamTypeClasses #-}
+
 import Graphics.X11.ExtraTypes.XF86
 import XMonad
 import XMonad.Actions.CopyWindow
+import XMonad.Actions.CycleWS
+import XMonad.Actions.MessageFeedback
 import XMonad.Config.Desktop
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.FadeInactive
 import XMonad.Hooks.ManageDocks
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.Fullscreen
 import XMonad.Layout.Grid
-import XMonad.Layout.NoBorders
 import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Layout.NoBorders
+import XMonad.Layout.Reflect
+import XMonad.Layout.Renamed
+import XMonad.Layout.ShowWName
+import XMonad.Layout.Simplest
 import XMonad.Layout.Spacing
+import XMonad.Layout.ToggleLayouts
 import XMonad.Util.Cursor
 import XMonad.Util.SpawnOnce
 import XMonad.Util.Run (safeSpawn)
@@ -22,6 +34,7 @@ import XMonad.Util.EZConfig (additionalKeys)
 
   * Less obtrusive active window indicator
   * New layouts (tabs, 3-column, flex, etc.)
+  * Try XMonad.Layout.BinarySpacePartition (currently have reflect support already)
   * Check out hidden layouts (Xmonad.Layout.Hidden)
   * Rework theme
   * Rework workspaces
@@ -59,16 +72,17 @@ myModMask = mod4Mask
 myTerminal = "termite"
 
 -- Workspaces.
-myws1 = "\xf120"
-myws2 = "\xf269"
-myws3 = "\xf121"
-myws4 = "\xf07b"
-myws5 = "\xf099"
-myws6 = "\xf1bc"
-myws7 = "\xf11b"
+myws1 = "term"
+myws2 = "gen"
+myws3 = "code"
+myws4 = "hb"
+myws5 = "hint"
+myws6 = "game"
+myws7 = "tmp"
 
+-- myWorkspaces = map show [1..9]
 myWorkspaces :: [String]
-myWorkspaces = [myws1, myws2, myws3, myws4, myws5, myws6 , myws7 ]
+myWorkspaces = [myws1, myws2, myws3, myws4, myws5, myws6, myws7]
 
 -- Startup.
 myStartupHook = do
@@ -103,6 +117,16 @@ myBorderWidth = 3
 myNormalBorderColor = "#2b303b"
 myFocusedBorderColor = "#bf616a"
 
+myWideFont  = "xft:Anonymous Pro:"
+  ++ "style=Regular:pixelsize=120:hinting=true"
+
+myShowWNameTheme = def
+  { swn_font              = myWideFont
+  , swn_fade              = 0.5
+  , swn_bgcolor           = "#000000"
+  , swn_color             = "#FFFFFF"
+  }
+
 -- Xmobar.
 myBar = "xmobar"
 myPP = xmobarPP { ppCurrent = xmobarColor "#bf616a" ""
@@ -117,16 +141,32 @@ myPP = xmobarPP { ppCurrent = xmobarColor "#bf616a" ""
 toggleStrutsKey XConfig {XMonad.modMask = modMask} = (modMask, xK_b)
 
 -- Layout.
-myLayoutHook = avoidStruts $ smartBorders ((spacing 5 $ (tall ||| grid)) ||| (noBorders Full ))
+myLayoutHook = showWorkspaceName
+  $ smartBorders
+  $ fullScreenToggle
+  $ mirrorToggle
+  $ reflectToggle
+  $ tall ||| grid
   where
-    tall = Tall 1 (3/100) (1/2)
-    grid = GridRatio (4/3)
+    fullScreenToggle = mkToggle (single FULL)
+    mirrorToggle = mkToggle (single MIRROR)
+    reflectToggle = mkToggle (single REFLECTX)
+    showWorkspaceName = showWName' myShowWNameTheme
+    named n = renamed [(XMonad.Layout.Renamed.Replace n)]
+    tall = named "Tall"
+      $ avoidStruts
+      $ spacing 5
+      $ Tall 1 (3/100) (1/2)
+    grid = named "Grid"
+      $ avoidStruts
+      $ spacing 5
+      $ GridRatio (4/3)
 
 -- Mangehooks.
 myManageHook = manageDocks
 
 -- Event Hooks.
-myEventHook = docksEventHook <+> fullscreenEventHook
+myEventHook = docksEventHook <+> XMonad.Hooks.EwmhDesktops.fullscreenEventHook
 
 -- Keyboard.
 -- ((modMask, xK_f ), runOrRaiseMaster "firefox" (className =? "Firefox"))
@@ -137,16 +177,27 @@ myAdditionalKeys =
     ((0, xF86XK_AudioMute             ), safeSpawn "pamixer" ["-t"]),
     ((0, xF86XK_MonBrightnessUp       ), safeSpawn "light" ["-A", "15"]),
     ((0, xF86XK_MonBrightnessDown     ), safeSpawn "light" ["-U", "15"]),
-    ((shiftMask, xF86XK_MonBrightnessUp     ), safeSpawn "/home/josh/bin/acdlight" ["-A", "100"]),
-    ((shiftMask, xF86XK_MonBrightnessDown   ), safeSpawn "/home/josh/bin/acdlight" ["-U", "100"]),
-    ((myModMask, xK_t                       ), safeSpawn "/home/josh/bin/tray" []),
-    ((mod1Mask, xK_space                    ), safeSpawn "rofi" ["-show", "run"]),
+    ((shiftMask, xF86XK_MonBrightnessUp   ), safeSpawn "/home/josh/bin/acdlight" ["-A", "100"]),
+    ((shiftMask, xF86XK_MonBrightnessDown ), safeSpawn "/home/josh/bin/acdlight" ["-U", "100"]),
+    ((myModMask, xK_t                     ), safeSpawn "/home/josh/bin/tray" []),
+    ((mod1Mask, xK_space                  ), safeSpawn "rofi" ["-show", "run"]),
     -- Toggle make focused window always visible
-    ((myModMask, xK_s), toggleCopyToAll)
+    ((myModMask, xK_s                     ), toggleCopyToAll),
+    ((myModMask, xK_l                     ), nextWS),
+    ((myModMask, xK_h                     ), prevWS),
+    ((myModMask .|. shiftMask, xK_l       ), sendMessage Expand),
+    ((myModMask .|. shiftMask, xK_h       ), sendMessage Shrink),
+    ((myModMask, xK_f                     ), sendMessage (XMonad.Layout.MultiToggle.Toggle FULL)),
+    ((myModMask, xK_r                     ), tryMsgR (Rotate) (XMonad.Layout.MultiToggle.Toggle REFLECTX)),
+    ((myModMask .|. shiftMask, xK_r       ), sendMessage (XMonad.Layout.MultiToggle.Toggle REFLECTX)),
+    ((myModMask, xK_m                     ), sendMessage (XMonad.Layout.MultiToggle.Toggle MIRROR))
   ] where
       toggleCopyToAll = wsContainingCopies >>= \ws -> case ws of
         [] -> windows copyToAll
         _ -> killAllOtherCopies
+
+      -- try sending one message, fallback if unreceived, then refresh
+      tryMsgR x y = sequence_ [(tryMessage_ x y), refresh]
 
 -- Main configuration, override the defaults to your liking.
 myConfig = baseConfig {
