ee3fd937046ac86a03ef50e4703a196124b0c38d
Joshua Wood <josh@hintmedia.com>
2017-05-06 14:43:53 -0700

    Move some scripts to xmobar plugins.

--- tag-arch/xmonad/xmonad.hs
+++ tag-arch/xmonad/xmonad.hs
@@ -109,9 +109,47 @@ myLogHook = do
   where
     fadeAmount = 0.85
 
--- Style.
-yellow  = "#b58900"
-red = "#dc322f"
+--------------------------------------------------------------------------
+-- Style                                                                --
+--------------------------------------------------------------------------
+{-|
+
+  Color Reference
+
+  The following colors were measured inside Atom using its built-in inspector.
+
+  +---------------------------------------------+
+  |  Color Name  |         RGB        |   Hex   |
+  |--------------+--------------------+---------|
+  | Black        | rgb(40, 44, 52)    | #282c34 |
+  |--------------+--------------------+---------|
+  | White        | rgb(171, 178, 191) | #abb2bf |
+  |--------------+--------------------+---------|
+  | Light Red    | rgb(224, 108, 117) | #e06c75 |
+  |--------------+--------------------+---------|
+  | Dark Red     | rgb(190, 80, 70)   | #be5046 |
+  |--------------+--------------------+---------|
+  | Green        | rgb(152, 195, 121) | #98c379 |
+  |--------------+--------------------+---------|
+  | Light Yellow | rgb(229, 192, 123) | #e5c07b |
+  |--------------+--------------------+---------|
+  | Dark Yellow  | rgb(209, 154, 102) | #d19a66 |
+  |--------------+--------------------+---------|
+  | Blue         | rgb(97, 175, 239)  | #61afef |
+  |--------------+--------------------+---------|
+  | Magenta      | rgb(198, 120, 221) | #c678dd |
+  |--------------+--------------------+---------|
+  | Cyan         | rgb(86, 182, 194)  | #56b6c2 |
+  |--------------+--------------------+---------|
+  | Gutter Grey  | rgb(99, 109, 131)  | #636d83 |
+  |--------------+--------------------+---------|
+  | Comment Grey | rgb(92, 99, 112)   | #5c6370 |
+  +---------------------------------------------+
+
+|-}
+
+yellow  = "#d19a66 "
+red = "#be5046"
 
 myBorderWidth = 3
 myNormalBorderColor = "#2b303b"
@@ -129,12 +167,12 @@ myShowWNameTheme = def
 
 -- Xmobar.
 myBar = "xmobar"
-myPP = xmobarPP { ppCurrent = xmobarColor "#bf616a" ""
-                     , ppHidden = xmobarColor "#c0c5ce" ""
+myPP = xmobarPP { ppCurrent = xmobarColor "#e06c75" ""
+                     , ppHidden = xmobarColor "#abb2bf" ""
                      , ppHiddenNoWindows = xmobarColor "#4f5b66" ""
                      , ppUrgent = xmobarColor "#a3be8c" ""
                      , ppLayout = xmobarColor "#4f5b66" ""
-                     , ppTitle =  xmobarColor "#c0c5ce" "" . shorten 80
+                     , ppTitle =  xmobarColor "#abb2bf" "" . shorten 80
                      , ppSep = xmobarColor "#4f5b66" "" "  "
                      }
 
