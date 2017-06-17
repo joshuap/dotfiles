83266d6a1f451044ce47d9fd594ec4dd332f89c0
Joshua Wood <josh@joshuawood.net>
2017-06-17 13:15:24 -0700

    Fix dropbox/xcape startup issues (not using systemd for now).

--- tag-arch/xmonad/init.sh
+++ tag-arch/xmonad/init.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 
-# DISPLAY=":0"
-
 # ----------------------------------------------------------------------
 # Keyboard
 # ----------------------------------------------------------------------
 
 XKBDIR=$HOME/.config/xkb
-notify-send -u low "Keyboard initialized"
 xkbcomp -synch -w3 -I$XKBDIR $XKBDIR/keymap/my-keymap $DISPLAY
-(exec killall -q xcape) & # gets restarted by the xcape systemd user service
+xcape -e "Hyper_L=Tab;Hyper_R=backslash"
+notify-send -u low "Keyboard initialized"
