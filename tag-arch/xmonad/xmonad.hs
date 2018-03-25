5d7be5161703e74b1e765c96e890674b13ecdd50
Joshua Wood <josh@joshuawood.net>
2018-03-25 14:29:22 -0700

    Add combi mode to rofi.

--- tag-arch/xmonad/xmonad.hs
+++ tag-arch/xmonad/xmonad.hs
@@ -246,7 +246,7 @@ myAdditionalKeys =
     ((0, xF86XK_MonBrightnessDown     ), safeSpawn "light" ["-U", "15"]),
     ((shiftMask, xF86XK_MonBrightnessUp   ), safeSpawn "/home/josh/bin/acdlight" ["-A", "100"]),
     ((shiftMask, xF86XK_MonBrightnessDown ), safeSpawn "/home/josh/bin/acdlight" ["-U", "100"]),
-    ((mod1Mask, xK_space                  ), safeSpawn "rofi" ["-show", "run"]),
+    ((mod1Mask, xK_space                  ), safeSpawn "rofi" ["-show", "combi"]),
 
     -- Toggle make focused window always visible
     ((myModMask, xK_s ), toggleCopyToAll),
