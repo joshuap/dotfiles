053bae429408d5634ee01ee7d5f64a277ed80591
Joshua Wood <josh@joshuawood.net>
2018-06-02 11:33:23 -0700

    Store wallpaper on dropbox.

--- tag-arch/lib/bin/wallpaper.sh
+++ tag-arch/lib/bin/wallpaper.sh
@@ -2,7 +2,7 @@
 
 # Sets a random wallpaper.
 
-WALLPAPER_DIR="$HOME/Pictures/Wallpaper/"
+WALLPAPER_DIR="$HOME/Dropbox/Wallpaper/"
 
 random_wallpaper() {
 	find "$WALLPAPER_DIR" -type f -or -type l | shuf -n1
