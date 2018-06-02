e799ae98d9a9e9be3e2679c7805ed08cb95f9e6f
Joshua Wood <josh@joshuawood.net>
2018-06-02 11:32:52 -0700

    Add more taffybar widgets.

--- tag-xmonad/config/taffybar/taffybar.hs
+++ tag-xmonad/config/taffybar/taffybar.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           System.Taffybar
+import           System.Taffybar.Hooks
 import           System.Taffybar.Information.CPU
 import           System.Taffybar.Information.Memory
 import           System.Taffybar.SimpleConfig
@@ -12,6 +13,14 @@ import           System.Taffybar.DBus
 import           Control.Monad.IO.Class
 import qualified Graphics.UI.Gtk as Gtk
 
+transparent = (0.0, 0.0, 0.0, 0.0)
+yellow1 = (0.9453125, 0.63671875, 0.2109375, 1.0)
+yellow2 = (0.9921875, 0.796875, 0.32421875, 1.0)
+green1 = (0, 1, 0, 1)
+green2 = (1, 0, 1, 0.5)
+red = (1, 0, 0, 1)
+taffyBlue = (0.129, 0.588, 0.953, 1)
+
 memCallback = do
   mi <- parseMeminfo
   return [memoryUsedRatio mi]
@@ -22,15 +31,16 @@ cpuCallback = do
 
 main = do
   let memCfg = defaultGraphConfig
-               { graphDataColors = [(1, 0, 0, 1)]
+               { graphDataColors = [red]
                , graphLabel = Just "mem"
                }
       cpuCfg = defaultGraphConfig
-               { graphDataColors =
-                   [ (0, 1, 0, 1)
-                   , (1, 0, 1, 0.5)
-                   ]
+               { graphDataColors = [green1, green2]
                , graphLabel = Just "cpu"
+               }
+      netCfg = defaultGraphConfig
+               { graphDataColors = [yellow1, yellow2]
+               , graphLabel = Just "net"
                }
   let clock = textClockNew Nothing "<span fgcolor='orange'>%a %b %_d %H:%M</span>" 1
       workspaces = workspacesNew defaultWorkspacesConfig
@@ -40,11 +50,12 @@ main = do
       mpris = mpris2New
       mem = pollingGraphNew memCfg 1 memCallback
       cpu = pollingGraphNew cpuCfg 0.5 cpuCallback
+      net = networkGraphNew netCfg Nothing
       -- tray = sniTrayNew
       tray = sniTrayNew >>= (\widget -> (liftIO $ Gtk.widgetShowAll widget) >> return widget)
-  dyreTaffybar $ withLogServer $ toTaffyConfig defaultSimpleTaffyConfig
+  dyreTaffybar $ withBatteryRefresh $ withLogServer $ toTaffyConfig defaultSimpleTaffyConfig
                    { startWidgets = [ workspaces, layout, windows, note ]
-                   , endWidgets = map (>>= buildPadBox) [ tray, clock, mem, cpu, mpris ]
+                   , endWidgets = map (>>= buildPadBox) [ batteryIconNew, clock, tray, mem, cpu, net, mpris ]
                    -- , endWidgets = [ clock, mem, cpu, mpris ]
-                   , barHeight = 40
+                   , barHeight = 45
                    }
